d41c35ab0b8fff8417b03887f147b157bfd514cf
Old-Lx <19-105557@@usb.ve>
2022-08-23 16:10:13 -0400

    23/08 update

--- synbioven.py
+++ synbioven.py
@@ -5,20 +5,34 @@ from Bio import SeqIO
 #Nótese que apenas estoy empezando a usar esta librería, cualquier
 #recomendación será bien recibida
 
+def com_fasta(archivo):
+    print("Ejecutando...")
+    secs=list(SeqIO.parse(archivo, "fasta"))
+    with open("sec_sin_rep.fasta", "w") as new_fasta:
+        for sec_n in secs:
+            for ind_2, adn_2 in enumerate(SeqIO.parse(archivo, "fasta")):
+                if len(secs)!=ind_2:
+                    if sec_n.seq==adn_2[ind_2+1]:
+                        SeqIO.write(adn_2, new_fasta, "fasta")
+
+
 #Función usada para dividir todas las secuencias de un archivo
 #fasta en varios archivos fasta
-def div_fasta():
+def div_fasta(archivo):
     #estoy iterando sobre el archivo fasta para dividir cada secuencia
     #la librería que importé tiene todas las herramientas necesarias para
     #manipular archivos fasta
-    with open(input("Ruta del archivo fasta: ")) as handle:
-        for dna_seq in SeqIO.parse(handle, "fasta"):
-            f_name="secuencia_"+str(dna_seq.id)
-            with open(f_name+".fasta", "w") as cur_seq:
-                cur_seq.write(dna_seq)
+    print("Ejecutando...")
+    with open(archivo) as handle:
+        for indice, adn_sec in enumerate(SeqIO.parse(handle, "fasta")):
+            print(str(len(adn_sec.seq)))
+            break
+        '''with open(f_name+".txt", "w") as cur_seq:
+                cur_seq.write(dna_seq)'''
 
-
-
+if __name__ == '__main__':
+    div_fasta("Lemna_minor.fasta")
+    com_fasta("Lemna_minor.fasta")
 
 #antes de publicar, hay que citar a los autores de biopython
 #y a los cursos que lo explican en español
