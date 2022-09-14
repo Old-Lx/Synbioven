186576e379660f2e5c3988c8c116a3c672c3afe2
Old-Lx <19-10557@gmail.com>
2022-09-14 09:11:32 -0400

    Primer script listo

--- synbioven.py
+++ synbioven.py
@@ -5,8 +5,8 @@ from Bio import SeqIO
 #Nótese que apenas estoy empezando a usar esta librería, cualquier
 #recomendación será bien recibida
 
-#Esta función guarda un archivo fasta con las secuencias repetidas
-def recon_rep(archivo):
+#Esta función crea un nuevo fasta sin secuencias repetidas
+def limp_rep(archivo):
     print("Ejecutando...")
     secs=list(SeqIO.parse(archivo, "fasta"))
     with open("sec_rep.fasta", "w") as new_fasta:    
@@ -16,7 +16,7 @@ def recon_rep(archivo):
             for sec_n in secs[n:]:
                 if sec_n.seq==adn_2.seq:
                     rep_id[n]=sec_n.id
-                print(n)
+                print('Analizando secuencia '+n+' de '+str(len(secs)))
             n=n+1
             if len(secs)==n:
                 break
@@ -28,7 +28,7 @@ def recon_rep(archivo):
 #Esta función guarda un nuevo fasta con los mismos elementos que el original
 #pero excluyendo las secuencias repetidas que guardamos con recon_rep donde arc_comp es
 #el archivo completo y arc_sec_rep es el archivo con las secuencias que están repetidas
-def elim_rep(arc_comp, arc_sec_rep):
+'''def elim_rep(arc_comp, arc_sec_rep):
     print("Ejecutando...")
     secs=list(SeqIO.parse(arc_comp, "fasta"))
     with open(arc_sec_rep) as reps:    
@@ -52,13 +52,13 @@ def div_fasta(archivo):
         for indice, adn_sec in enumerate(SeqIO.parse(handle, "fasta")):
             print(str(len(adn_sec.seq)))
             break
-        '''with open(f_name+".txt", "w") as cur_seq:
+        with open(f_name+".txt", "w") as cur_seq:
                 cur_seq.write(dna_seq)'''
 
 if __name__ == '__main__':
-    div_fasta("Lemna_minor.fasta")
-    recon_rep("Lemna_minor.fasta")
-    elim_rep(str(), str())
+    limp_rep("Lemna_minor.fasta")
+    '''recon_rep("Lemna_minor.fasta")
+    elim_rep(str(), str())'''
 
 #antes de publicar, hay que citar a los autores de biopython
 #y a los cursos que lo explican en español
